523ff27be2a6774bab364b1f4fe3a8adb8f256ab
Muhammad Al-fatih Ritonga <muhammadalfatihritonga22@gmail.com>
2025-01-11 14:48:06 +0700

    package sort

--- sort.go
+++ sort.go
@@ -5,33 +5,33 @@ import (
 	"sort"
 )
 
-type User struct {
+type Person struct {
 	Name string
 	Age  int
 }
 
-type UserSlice []User
+type PersonSlice []Person
 
-func (a UserSlice) Len() int {
+func (a PersonSlice) Len() int {
 	return len(a)
 }
 
-func (a UserSlice) Swap(i, j int) {
+func (a PersonSlice) Swap(i, j int) {
 	a[i], a[j] = a[j], a[i]
 }
 
-func (a UserSlice) Less(i, j int) bool {
+func (a PersonSlice) Less(i, j int) bool {
 	return a[i].Age < a[j].Age
 }
 
 func main() {
-	users := []User{
+	persons := []Person{
 		{Name: "Fatih", Age: 22},
 		{Name: "Elma", Age: 25},
 		{Name: "Syakira", Age: 12},
 		{Name: "Syilfa", Age: 22},
 	}
 
-	sort.Sort(UserSlice(users))
-	fmt.Println(users)
+	sort.Sort(PersonSlice(persons))
+	fmt.Println(persons)
 }
